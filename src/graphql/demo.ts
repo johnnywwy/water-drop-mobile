cdc999b109f7ac0dbdf3b3d9e88210e0865ba13c
wuwenyu <johnnywwy@gmail.com>
2024-04-28 18:10:47 +0800

    feat：引入apollo

---
+++ src/graphql/demo.ts
@@ -0,0 +1,9 @@
+import { gql } from "@apollo/client";
+
+export const FIND = gql`
+  query find($id: String!) {
+    find(id: $id) {
+      name
+    }
+  }
+`;
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,12 @@
-import React from "react";
+// import React from "react";
 import ReactDOM from "react-dom/client";
+import { ApolloProvider } from "@apollo/client";
 import App from "./App";
 import "./index.css";
+import { client } from "./utils/apollo";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
-  <React.StrictMode>
+  <ApolloProvider client={client}>
     <App />
-  </React.StrictMode>,
+  </ApolloProvider>,
 );
