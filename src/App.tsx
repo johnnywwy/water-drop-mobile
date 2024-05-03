b772e1b7ebccbeee9fe95a9bb98fa878db7e91a7
johnnywwy <johnnywwy@gmail.com>
2024-05-03 17:59:36 +0800

    feat：reset css

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,47 @@
 // import "./App.css";
-
-import { useQuery } from "@apollo/client";
-import { Button } from "antd-mobile";
-import { FIND } from "./graphql/demo";
+// import { useQuery } from "@apollo/client";
+import { Button, Form, Input } from "antd-mobile";
+import { useMutation } from "@apollo/client";
+import { UPDATE } from "./graphql/demo";
 
 function App() {
-  const { loading, data } = useQuery(FIND, {
-    variables: {
-      id: "79b308ad-8ef7-4031-beb3-1b5065cfa6ec",
-    },
-  });
+  const [update] = useMutation(UPDATE);
+
+  const onClickHandle = (v) => {
+    console.log("onClickHandle", v);
+
+    update({
+      variables: {
+        id: "84770e80-4952-40c7-8772-375413ffaecb",
+        params: {
+          ...v,
+        },
+      },
+    });
+  };
 
   return (
     <div>
       <Button color="primary" fill="solid">
-        Solid
+        刷新
       </Button>
-      <p>loading: {`${loading}`}</p>
-      <p> data: {JSON.stringify(data)}</p>
+      <Form
+        onFinish={onClickHandle}
+        layout="horizontal"
+        footer={
+          <Button block type="submit" color="primary" size="large">
+            提交
+          </Button>
+        }
+      >
+        <Form.Header>水平布局表单</Form.Header>
+        <Form.Item name="name" label="用户名" rules={[{ required: true, message: "姓名不能为空" }]}>
+          <Input onChange={console.log} placeholder="请输入姓名" />
+        </Form.Item>
+        <Form.Item name="desc" label="描述" rules={[{ required: true, message: "描述不能为空" }]}>
+          <Input onChange={console.log} placeholder="请输入描述" />
+        </Form.Item>
+      </Form>
     </div>
   );
 }
